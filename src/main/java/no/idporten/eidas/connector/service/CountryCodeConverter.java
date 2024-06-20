b20c7be19600c7119633bcd598569cde40d95ddd
Anne Marte Hjemås <kons-ahj@digdir.no>
2024-06-20 14:14:33 +0200

    mappe demoland til svensk landskode (#24)

--- src/main/java/no/idporten/eidas/connector/service/CountryCodeConverter.java
+++ src/main/java/no/idporten/eidas/connector/service/CountryCodeConverter.java
@@ -5,7 +5,13 @@ import org.apache.commons.lang.StringUtils;
 
 public class CountryCodeConverter {
 
+    //we fake the country code for the demo land to be sweden for now
+    public static final String DEMOLAND = "CA";
+
     public static String getISOAlpha3CountryCode(String countryCode) {
+        if (DEMOLAND.equals(countryCode)) {
+            return "SWE";
+        }
         if (StringUtils.isNotEmpty(countryCode) && countryCode.length() == 2) {
             ULocale locale = new ULocale("und-" + countryCode);
             return locale.getISO3Country();
