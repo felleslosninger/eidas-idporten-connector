99db51223f5b8650eb17636c37a3524bc7aa6984
Anne Marte Hjemås <kons-ahj@digdir.no>
2024-09-18 12:29:06 +0200

    fjerne redundant mellomside

--- src/main/java/no/idporten/eidas/connector/web/CitizenCountryController.java
+++ src/main/java/no/idporten/eidas/connector/web/CitizenCountryController.java
@@ -30,15 +30,15 @@ public class CitizenCountryController {
 
     @PostMapping
     public ModelAndView submitSelection(@ModelAttribute(CITIZEN_FORM) CitizenCountryForm form,
+                                        @SessionAttribute(SESSION_ATTRIBUTE_AUTHORIZATION_REQUEST) PushedAuthorizationRequest authorizationRequest,
                                         @RequestParam("action") String action) {
         ModelAndView modelAndView = new ModelAndView();
 
         if ("ok".equals(action)) {
-            // Logic for save action
-            modelAndView.setViewName("confirmation");
-            modelAndView.addObject("selectedCountry", form.getCountryId());
-            form.setCountryId(form.getCountryId());
-            modelAndView.addObject(CITIZEN_FORM, form);
+            String selectedCountry = form.getCountryId().toUpperCase();
+            auditService.auditCountrySelection(selectedCountry);
+            modelAndView.setViewName(createLightRequest(selectedCountry, authorizationRequest));
+
         } else if ("cancel".equals(action)) {
             modelAndView.setViewName("redirect:/some-other-page");
         } else {
@@ -48,25 +48,6 @@ public class CitizenCountryController {
         return modelAndView;
     }
 
-    @PostMapping("/confirm")
-    public ModelAndView confirmation(@ModelAttribute(CITIZEN_FORM) CitizenCountryForm form,
-                                     @SessionAttribute(SESSION_ATTRIBUTE_AUTHORIZATION_REQUEST) PushedAuthorizationRequest authorizationRequest,
-                                     @RequestParam("action") String action) {
-        ModelAndView modelAndView = new ModelAndView();
-
-        if ("confirm".equals(action)) {
-            String selectedCountry = form.getCountryId().toUpperCase();
-            auditService.auditCountrySelection(selectedCountry);
-            modelAndView.setViewName(createLightRequest(selectedCountry, authorizationRequest));
-        } else if ("back".equals(action)) {
-            modelAndView.addObject(CITIZEN_FORM, new CitizenCountryForm());
-            modelAndView.setViewName("selector");
-        } else {
-            modelAndView.setViewName(ERROR);
-            modelAndView.addObject(ERROR, "Unknown action: %s".formatted(action));
-        }
-        return modelAndView;
-    }
 
     private String createLightRequest(String selectedCountry, PushedAuthorizationRequest pushedAuthorizationRequest) {
         LightRequest lightRequest = specificConnectorService.buildLightRequest(selectedCountry, pushedAuthorizationRequest);
